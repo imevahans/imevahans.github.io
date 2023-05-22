6a83b795a5b1d300da1dd35ba7f92a69e5ff9f56
imevahans <ImEvaHans@gmail.com>
2023-05-22 10:59:40 +0800

    Restarting

--- src/components/Heading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Heading(props) {
-    return (
-        <h1 className='heading' style={{color: props.color}}> {props.title} </h1>
-    )
-}
-
-export default Heading;
--- src/components/Heading2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Heading2(props) {
-    return (
-        <h2 className='heading2' style={{color: props.color}}> {props.title} </h2>
-    )
-}
-
-export default Heading2;
